e808c0ca9871516e481aee926312a6b55c99edaf
Lovinder Pnag <lspnag@gmail.com>
2016-12-20 07:37:03 -0800

    use phone number name lookup

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -3,7 +3,17 @@ require 'sinatra'
 require 'twilio-ruby'
 
 get '/hello-monkey' do
+  people = {
+    '+12099883115' => 'Lovinder',
+    '+14158675309' => 'Vladimir Putin',
+    '+14158675310' => 'Angela Merkel',
+    '+14158675311' => 'Barack Obama',
+    '+14158675312' => 'Donald Trump',
+  }
+
+  name = people[params['From']] || 'Monkey'
+
   Twilio::TwiML::Response.new do |r|
-    r.Say 'How Many Monkeys Jumping on the Tree'
+    r.Say "How Many Monkeys Jumping on the Tree #{name}"
   end.text
 end

db14e0eecb72a715cd7a5169a0d5fb71a9af24ea
Lovinder Pnag <lspnag@gmail.com>
2016-12-20 15:19:44 -0800

    hello monkey

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -5,16 +5,13 @@ require 'twilio-ruby'
 get '/hello-monkey' do
   people = {
     '+12099883115' => 'Lovinder',
-    '+14158675309' => 'Vladimir Putin',
-    '+14158675310' => 'Angela Merkel',
-    '+14158675311' => 'Barack Obama',
-    '+14158675312' => 'Donald Trump',
+    '+14158675310' => 'Boots',
+    '+14158675311' => 'Virgil',
+    '+14158675312' => 'Marcel',
   }
-
   name = people[params['From']] || 'Monkey'
-
   Twilio::TwiML::Response.new do |r|
-    r.Say "How Many Monkeys Jumping on the Tree #{name}"
+    r.Say "Hello #{name}"
     r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
     r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
       g.Say 'To speak to a real monkey, press 1.'
@@ -24,11 +21,12 @@ get '/hello-monkey' do
   end.text
 end
 
+
 get '/hello-monkey/handle-gather' do
-  redirect '/hello-monkey' unless ['1','2'].include? (params['Digits'])
+  redirect '/hello-monkey' unless ['1', '2'].include?(params['Digits'])
   if params['Digits'] == '1'
     response = Twilio::TwiML::Response.new do |r|
-      r.Dial '+13105551212' ### Connect the caller to Koko, or your cell
+      r.Dial '+13105551212'
       r.Say 'The call failed or the remote party hung up. Goodbye.'
     end
   elsif params['Digits'] == '2'
@@ -47,3 +45,4 @@ get '/hello-monkey/handle-record' do
     r.Say 'Goodbye.'
   end.text
 end
+

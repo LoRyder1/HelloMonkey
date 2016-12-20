a086f90f023fa06f2fa4b495363fce820f88a8d8
Lovinder Pnag <lspnag@gmail.com>
2016-12-20 08:06:40 -0800

    fix bug

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -16,7 +16,7 @@ get '/hello-monkey' do
   Twilio::TwiML::Response.new do |r|
     r.Say "How Many Monkeys Jumping on the Tree #{name}"
     r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
-    r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do
+    r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
       g.Say 'To speak to a real monkey, press 1.'
       g.Say 'Press any other key to start over.'
     end
@@ -25,7 +25,7 @@ end
 
 get '/hello-monkey/handle-gather' do
   redirect '/hello-monkey' unless params['Digits'] == '1'
-  Twilio::TwiML:Response.new do |r|
+  Twilio::TwiML::Response.new do |r|
     r.Dial '+13105551212'
     r.Say 'The call failed or the remote party hung up. Goodbye.'
   end.text
